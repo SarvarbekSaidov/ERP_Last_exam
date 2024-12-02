5ec6d9d0c2043c361535ed947517bb1f502a2b87
Saidov Sarvarbek <forgivensinner6@gmail.com>
2024-12-02 08:45:22 +0500

    cmt

--- main/serializers.py
+++ main/serializers.py
@@ -2,17 +2,25 @@ from rest_framework import serializers
 from .models import Course, Lesson, Comment
 
 class CourseSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Course model.
+    """
     class Meta:
         model = Course
         fields = '__all__'
 
 class LessonSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Lesson model.
+    """
     class Meta:
         model = Lesson
         fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model.
+    """
     class Meta:
         model = Comment
         fields = '__all__'
-
